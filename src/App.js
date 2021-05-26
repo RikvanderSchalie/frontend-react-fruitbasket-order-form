7a54263f7d24c453f7e5f58a05323cf41122de61
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-05-26 11:43:17 +0200

    AddButtonThatCounts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import Fruitbutton from "./Fruitbutton"
+
+
+/*Randvoorwaarden fruit-counters
+Je maakt gebruik van de useState hook van React om de hoeveelheden bij te houden
+Klanten hebben de vrijheid om te kiezen tussen vier typen fruit:
+Bananen  : 🍌
+Aarbeien : 🍓
+Appels   : 🍏
+Kiwi's   : 🥝
+[] Elk type fruit heeft twee buttons: één die de hoeveelheid verhoogd (+) en één die de hoeveelheid verlaagd (-).
+[] De hoeveelheid geselecteerde fruittypes wordt bij iedere update getoond aan de gebruiker
+[] Er is ook een reset button, die alle counters weer op 0 zet.
+[] check react-hook npmsite mbt functie formulier */
+
 
 function App() {
   return (
-    <>
+    <div className>
       <h1>Fruitmand bezorgservice</h1>
-    </>
+
+        <Fruitbutton name="BANAAN"/>
+        <Fruitbutton name="AARDBEI"/>
+        <Fruitbutton name="APPEL"/>
+        <Fruitbutton name="SKIWI"/>
+
+    </div>
   );
 }
 
---
+++ src/Fruitbutton.js
@@ -0,0 +1,20 @@
+import { useState}  from 'react'
+
+function  Fruitbutton (props){
+
+    //useState is een hook, een hook begint altijd met "use"
+    //een hook is een loop
+
+const [count, setCount] = useState(0)
+
+    return ( <div>
+        {props.name} : {count}
+            { }
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+
+    </div>
+    )
+}
+
+export default Fruitbutton
